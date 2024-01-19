193d09515c6e90a9d59514e5a42a230c2f26ea9b
MadhushaPrasad <madushaprasad21@gmail.com>
2024-01-19 15:45:40 +0530

    Initial Commit

---
+++ Xcode Tutorial Demo/Xcode_Tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_DemoApp.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Madhusha Prasad on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
