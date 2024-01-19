c27217301e7a3f549f35ecfc5c5315304be0ef70
MadhushaPrasad <madushaprasad21@gmail.com>
2024-01-19 17:29:48 +0530

    my first ios app

--- Xcode Tutorial Demo/ContentView.swift
+++ Xcode Tutorial Demo/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Tap on me") {
+                print("Button Clicked")
+            }
+            Image("minion").imageScale(.large).cornerRadius(20)
+            Text("Hello Friend, Let's play hide and seek").foregroundColor(.black).font(.caption).fontWeight(.bold).multilineTextAlignment(.center)
         }
         .padding()
     }
